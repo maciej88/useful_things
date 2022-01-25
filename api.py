cf6fe33d97183adfaa27af19e58ee192fe266e9f
maciej <mkrywult88@gmail.com>
2022-01-25 18:30:04 +0100

    API - no link and key

--- api.py
+++ api.py
@@ -1,1 +1,18 @@
 import requests
+
+API_KEY = "api key space"
+BASE_URL = 'url hhtp'
+
+city = input("enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "celsius")
+else:
+    print("An error occurred.")
