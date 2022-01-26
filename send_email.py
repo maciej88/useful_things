712b269591979d8d7954240f6e028aa402b4c6d6
maciej <mkrywult88@gmail.com>
2022-01-26 18:41:45 +0100

    sending email

---
+++ send_email.py
@@ -0,0 +1,25 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+subject = "Email form python"
+body = "some text to send"
+sender_email = "zzz@email.com"
+receiver_email = "xxx@email.com"
+password = input("enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+print("Sending!")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+print("Done!")
