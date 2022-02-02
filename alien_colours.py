9e0c5838d3222be5d27b32217d8e17243da60b22
maciej <mkrywult88@gmail.com>
2022-02-02 18:12:29 +0100

    alien colours

---
+++ alien_colours.py
@@ -0,0 +1,13 @@
+alien_color = 'blue'
+
+if alien_color == 'green':
+    # player_points += 5
+    print("you get 5 points!")
+if alien_color == 'yellow':
+    # player_points += 10
+    print("you get 10 points!")
+if alien_color == 'red':
+    # player_points += 15
+    print("you get 15 points!")
+else:
+    pass
