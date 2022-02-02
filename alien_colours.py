1e2f4552c3a83d6d38f3119a837e9d2429af2abb
maciej <mkrywult88@gmail.com>
2022-02-02 18:24:09 +0100

    more useful points counter

--- alien_colours.py
+++ alien_colours.py
@@ -1,13 +1,14 @@
 alien_color = 'red'
+player_points = 0
 
 if alien_color == 'green':
-    # player_points += 5
-    print("you get 5 points!")
+    player_points += 5
+    print(f"you get {player_points} points!")
 elif alien_color == 'yellow':
-    # player_points += 10
-    print("you get 10 points!")
+    player_points += 10
+    print(f"you get {player_points} points!")
 elif alien_color == 'red':
-    # player_points += 15
-    print("you get 15 points!")
+    player_points += 15
+    print(f"you get {player_points} points!")
 else:
     pass
