129684c0c52b9f5315877dfa15278021a05921ec
maciej <mkrywult88@gmail.com>
2022-02-03 19:34:56 +0100

    aliens again

--- aliens.py
+++ aliens.py
@@ -13,6 +13,12 @@ for alien in range(30):
     new_alien = {'color': 'zielony', 'points': 5, 'speed': 'wolno'}
     aliens.append(new_alien)
 
+for alien in aliens[0:3]:
+    if alien['color'] == 'zielony':
+        alien['color'] = 'żółty'
+        alien['points'] = 10
+        alien['speed'] = 'średnio'
+
 for alien in aliens[:5]:
     print(alien)
 print('-----------------------------------')
