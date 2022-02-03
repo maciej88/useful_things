b87f9df4a27e852b01f90566b273079d789459cc
maciej <mkrywult88@gmail.com>
2022-02-03 18:56:07 +0100

    aliens

---
+++ aliens.py
@@ -0,0 +1,19 @@
+alien0 = {'color': 'zielony', 'points': 5}
+alien1 = {'color': 'żółty', 'points': 10}
+alien2 = {'color': 'czerwony', 'points': 15}
+
+aliens = [alien0, alien1, alien2]
+
+for alien in aliens:
+    print(alien)
+
+aliens =[]
+
+for alien in range(30):
+    new_alien = {'color': 'zielony', 'points': 5, 'speed': 'wolno'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+print('-----------------------------------')
+print(f"Całkowita liczba obcych : {len(aliens)}")
