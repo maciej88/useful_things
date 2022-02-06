3d9ede1560e72dd81da6d7febd5e23669261b18f
maciej <mkrywult88@gmail.com>
2022-02-06 18:37:43 +0100

    inheritance

--- car.py
+++ car.py
@@ -23,6 +23,14 @@ class Car():
         """incrementation"""
         self.odometer_reading += kilometers
 
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        print(f"Ten samochod ma baterie o pojemnosci {self.battery_size} kWh.")
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
@@ -41,3 +49,7 @@ my_used_car.read_odometer()
 
 my_used_car.increment_odometer(10000)
 my_used_car.read_odometer()
+
+my_tesla = ElectricCar('tesla', 'model s', 2020)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
