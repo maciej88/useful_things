0efebf1067200a4b2d6c54ec7983863e177b3758
maciej <mkrywult88@gmail.com>
2022-02-06 18:13:01 +0100

    value modification

--- car.py
+++ car.py
@@ -15,6 +15,16 @@ class Car():
         """odometer info"""
         print(f"This car, done distance of {self.odometer_reading} km")
 
+    def update_odometer(self, milage):
+        """add value to odometer"""
+        self.odometer_reading = milage
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+my_new_car.odometer_reading = 23 #modification with no metod
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(23) #modification with metod
 my_new_car.read_odometer()
