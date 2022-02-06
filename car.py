39f4ddad7926e0e1c9f427ca663e28e056d048f5
maciej <mkrywult88@gmail.com>
2022-02-06 18:09:02 +0100

    default value for attr.

--- car.py
+++ car.py
@@ -4,11 +4,17 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Car description"""
         long_name = f"{str(self.year)} {self.make} {self.model}"
         return long_name.title()
 
+    def read_odometer(self):
+        """odometer info"""
+        print(f"This car, done distance of {self.odometer_reading} km")
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
