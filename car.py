f1eec9b8fdfc4d5d4af5f19ec794d078307d7a46
maciej <mkrywult88@gmail.com>
2022-02-06 18:05:58 +0100

    simple class with object

---
+++ car.py
@@ -0,0 +1,14 @@
+class Car():
+    def __init__(self, make, model, year):
+        """initializing attributes"""
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_descriptive_name(self):
+        """Car description"""
+        long_name = f"{str(self.year)} {self.make} {self.model}"
+        return long_name.title()
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
