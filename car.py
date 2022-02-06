ee994f708b3e39dd13eb21de83716b911a31e42d
maciej <mkrywult88@gmail.com>
2022-02-06 18:55:47 +0100

    inheritance is fun!

--- car.py
+++ car.py
@@ -23,13 +23,31 @@ class Car():
         """incrementation"""
         self.odometer_reading += kilometers
 
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery_size = 70
+class Battery():
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
 
     def describe_battery(self):
         print(f"Ten samochod ma baterie o pojemnosci {self.battery_size} kWh.")
+
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = f"Range this car is {range}km"
+        message += " after full charging"
+        print(message)
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+    def fill_gas_tank():
+        print("No fuel needed!")
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
@@ -52,4 +70,5 @@ my_used_car.read_odometer()
 
 my_tesla = ElectricCar('tesla', 'model s', 2020)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
