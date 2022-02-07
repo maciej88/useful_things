ae88f68cd6ac74672a7e00707e508cde0c8f9ba0
maciej <mkrywult88@gmail.com>
2022-02-07 18:47:31 +0100

    if __name__ == "__main__":

--- my_electric_car.py
+++ my_electric_car.py
@@ -1,6 +1,7 @@
 from car import ElectricCar
 
-my_tesla = ElectricCar('tesla', 'model s', 2020)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+if __name__ == "__main__":
+    my_tesla = ElectricCar('tesla', 'model s', 2020)
+    print(my_tesla.get_descriptive_name())
+    my_tesla.battery.describe_battery()
+    my_tesla.battery.get_range()
