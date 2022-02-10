80b08928bdfb5bafd01990fd5f244dc7e385e997
maciej <mkrywult88@gmail.com>
2022-02-10 19:37:40 +0100

    test repair

--- test_name_function.py
+++ test_name_function.py
@@ -1,12 +1,20 @@
 import unittest
+
 from name_function import get_formatted_name
 
 class NamesTestCase(unittest.TestCase):
-    """Test for 'name_function.py'"""
+    """Tests for 'name_function.py'."""
 
     def test_first_last_name(self):
-        """Data are supported?"""
-        formtted_name = get_formatted_name('Frank', 'sinatra')
-        self.assert_(formtted_name, 'Frank', 'Sinatra')
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
 
-unittest.main()
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name(
+            'wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
+if __name__ == '__main__':
+    unittest.main()
